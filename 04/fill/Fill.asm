9c60c7d9e213715df776bd357de20517c540efe7
Yuki Michishita <michishita.ocu@gmail.com>
2021-09-23 19:23:58 +0900

    4章・5章 完了

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,54 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(LOOP)
+    // i = 0
+    @i
+    M=0
+ 
+    //if M[KBD]>0 then goto SCREEN_BRACK
+    @KBD
+    D=M
+    @SCREEN_BLACK
+    D;JGT
+
+    (SCREEN_WHITE)
+        //M[SCREEN+i]=0
+        @i
+        D=M
+        @SCREEN
+        A=D+A
+        M=0
+        //i++
+        @i
+        M=M+1
+        @i
+        D=M
+        //i<8192 then goto SCREEN_WHITE
+        @8191
+        D=D-A
+        @SCREEN_WHITE
+        D;JLE
+        @LOOP
+        0;JMP
+
+    (SCREEN_BLACK)
+        //M[SCREEN+i]=-1
+        @i
+        D=M
+        @SCREEN
+        A=D+A
+        M=-1
+        //i++
+        @i
+        M=M+1
+        @i
+        D=M
+        //i<8192 then goto SCREEN_BLACK
+        @8191
+        D=D-A
+        @SCREEN_BLACK
+        D;JLE
+        @LOOP
+        0;JMP
+         
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,26 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+@2
+M=0
+
+(LOOP)
+    // if M[1] <= 0 goto END
+    @1
+    D=M
+    @END
+    D;JLE
+    // M[2] += M[0]
+    @0
+    D=M
+    @2
+    M=D+M
+    // M[1] -= 1
+    @1
+    M=M-1
+    // goto LOOP
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
